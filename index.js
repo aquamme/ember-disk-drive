085f8de9cccdb166067b7bcc342f1d689351b62f
Aaron Quamme <aaron.quamme@gmail.com>
2016-02-01 15:59:37 -0600

    Fix for nested addon asset inclusion, bump to 0.0.3

--- index.js
+++ index.js
@@ -2,5 +2,18 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-disk-drive'
+  name: 'ember-disk-drive',
+
+  included: function (app) {
+    this._super.included(app);
+
+    // Hack to ensure nested addons are able to import their deps
+    // See: https://github.com/hhff/spree-ember/blob/d9fc069aaf128e07593bd8dfc822926d85de92e0/packages/storefront/index.js#L9-L17
+    // and https://github.com/ember-cli/ember-cli/issues/3718
+    this.addons.forEach(function (addon) {
+      if (addon.name.indexOf('fetch') >= 0) {
+        addon.included.call(addon, app);
+      }
+    });
+  }
 };
