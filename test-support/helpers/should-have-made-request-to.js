0c8082d2df2fca0202b028ac06ae410c4597cfd4
Aaron Quamme <aquamme@fbsdata.com>
2016-03-21 11:50:18 -0500

    Create `shouldHaveMadeRequestTo` helper

---
+++ test-support/helpers/should-have-made-request-to.js
@@ -0,0 +1,24 @@
+import Ember from 'ember';
+
+function includesQueryParams(url, queryParams) {
+  return Object.keys(queryParams).map(key => `${key}=${queryParams[key]}`).every(param => url.includes(param));
+}
+
+export default Ember.Test.registerAsyncHelper('shouldHaveMadeRequestTo', function(app, assert, method, path, queryParams) {
+  let fetchService = app.__container__.lookup('service:fetch');
+
+  let recordings = fetchService.get('__recordings');
+
+  if (!recordings) {
+    throw 'ember-disk-drive: no recordings found. Did you remember to wrap your test with `DiskDrive.useRecording`?';
+  }
+
+  let match = !!Object.keys(recordings).find(key => {
+    if (key.includes(method.toUpperCase()) && key.includes(path) && includesQueryParams(key, queryParams)) {
+      return true;
+    }
+    return false;
+  });
+
+  assert.ok(match, `A request to "${path}" was made with the supplied query parameters`);
+});
